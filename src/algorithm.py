3f029cef603f65a14552b3ce63adb1de9f8b24e9
egiron1024 <108362511+egiron1024@users.noreply.github.com>
2024-09-26 19:50:26 -0400

    Update algorithm.py, fixed calculations from local env

--- src/algorithm.py
+++ src/algorithm.py
@@ -5,32 +5,46 @@ def calculate_payoff(strategies):
     num_cars = strategies.count("car")
     num_buses = strategies.count("bus")
 
-    final_points = [0] * n_players
+    payoffs = []
 
-    for i in range(n_players):
-        if strategies[i] == 'car':
-            final_points[i] = max(2 - num_cars, 0) + 2
-        elif strategies[i] == 'bus':
-            final_points[i] = 3 + num_buses
+    for strat in strategies:
+        if num_cars == n_players:
+            payoffs.append(2)
+        elif num_cars == 1 and strat == "car":
+            payoffs.append(4)
+        elif num_buses == 1 and strat == "bus":
+            payoffs.append(1)
+        elif num_buses == n_players:
+            payoffs.append(3)
+        else:
+            payoffs.append(3)
 
-    return final_points
-    
+    return payoffs
+
+
 
 def main():
-    n = int(input("Enter teh number of students going to school?"))
+    n = input("Enter the number of students going to school? ")
+    while not n.isdigit():
+        print("Invalid input! Please try again.")
+        n = input("Enter the number of students going to school? ")
+
+    n = int(n)
     player_choies = []
 
     for i in range(n):
-        strategy = input(f"Player {i+1}, choose your mode of transportation, car or bus? ")
+        strategy = input(f"Student {i+1}, choose your mode of transportation, car or bus? ")
         while strategy not in player_strategies:
             print("Invalid mode of transportation! Please try again.")
-            strategy = input(f"Player {i+1}, choose your mode of transportation, car or bus? ")
+            strategy = input(f"Student {i+1}, choose your mode of transportation, car or bus? ")
         player_choies.append(strategy)
 
     game_points = calculate_payoff(player_choies)
 
     for i in range(n):
-        print(f"Player {i+1} chose {player_choies[i]}. Payoff: {game_points[i]}")
+        print(f"Student {i+1} chose {player_choies[i]}. Payoff: {game_points[i]}")
+
+
 
 if __name__ == "__main__":
     main()
