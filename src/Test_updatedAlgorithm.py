95fc8b673d4a4020cb3a09832a768df7c660b191
meganarb <meganarb77@gmail.com>
2024-10-21 16:58:29 -0400

    added a couple test cases

--- src/Test_updatedAlgorithm.py
+++ src/Test_updatedAlgorithm.py
@@ -19,3 +19,38 @@ class TestCalculateRoadFunc(unittest.TestCase):
         road.add_car(10)
 
         self.assertEqual(calculate_road(road), 0.35)
+
+
+    def test2(self):
+        road = Road(10)
+
+        road.add_buses(1)
+        road.add_buses(2)
+        road.add_buses(3)
+        road.add_buses(4)
+        road.add_buses(5)
+        road.add_buses(6)
+        road.add_buses(7)
+
+        road.add_car(8)
+        road.add_car(9)
+        road.add_car(10)
+
+        self.assertEqual(calculate_road(road), 0.7)
+
+    def test3(self):
+        road = Road(40)
+
+        road.add_buses(1)
+        road.add_buses(2)
+        road.add_buses(3)
+        road.add_buses(4)
+        road.add_buses(5)
+        road.add_buses(6)
+        road.add_buses(7)
+
+        road.add_car(8)
+        road.add_car(9)
+        road.add_car(10)
+
+        self.assertEqual(calculate_road(road), .175)
