143ee7f6d407529935e194f1616a69a3270e2b90
meganarb <meganarb77@gmail.com>
2024-10-23 15:32:00 -0400

    added test case for bus payoff

--- src/Test_updatedAlgorithm.py
+++ src/Test_updatedAlgorithm.py
@@ -54,3 +54,23 @@ class TestCalculateRoadFunc(unittest.TestCase):
         road.add_car(10)
 
         self.assertEqual(calculate_road(road), .175)
+
+
+class TestCalculateBusPayoff(unittest.TestCase):
+
+    def test1(self):
+        road = Road(20)
+
+        road.add_buses(1)
+        road.add_buses(2)
+        road.add_buses(3)
+        road.add_buses(4)
+        road.add_buses(5)
+        road.add_buses(6)
+        road.add_buses(7)
+
+        road.add_car(8)
+        road.add_car(9)
+        road.add_car(10)
+
+        self.assertEqual(calculate_bus_payoff(road), 0.5)
